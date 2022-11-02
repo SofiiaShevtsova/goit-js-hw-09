7c5f0a3e570abb9f73d6ec3c05a73197b22565f7
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-11-02 09:31:22 +0200

    do 1 task all

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+let timeId = null;
 
+stopBtn.disabled = true;
+
+startBtn.addEventListener("click", (event) => {
+  startBtn.disabled = true;
+  timeId = setInterval(onClickStartBtn, 1000);
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener("click", (event) => {
+  stopBtn.disabled = true;
+  clearInterval(timeId);
+  startBtn.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStartBtn() {
+  body.style.backgroundColor = getRandomHexColor();
+}
